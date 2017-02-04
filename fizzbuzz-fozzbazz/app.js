b444e0c1afdc2e2b8efff89b689102d4f22afda6
ENGMENJARI <anjari1981@gmail.com>
2017-02-04 09:16:02 +0200

    c2

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -18,6 +18,20 @@ angular.module('app', ['ngRoute'])
 /*  HINT: Make sure your controllers, methods, and variables 
     are named what $routeProvider and the partials are expecting  */
 
-  .factory('counter', function(){})
-  .controller('fizzbuzzCtrl', function(){})
+  .factory('counter', function($routeProvider){
+    //first Initialize counter with a value of 0
+    var counter=0
+  })
+  .controller('fizzbuzzCtrl', function('counter'){
+    //if counter accept to be divided on three then I will display fizz
+    if(counter%3===0){
+      redirectTo:'/fizzbuzz'
+    }else if(counter%5===0){
+      //If the counter is divisible by five, display BUZZ
+      redirectTo:'/BUZZ'
+    }else if(counter%3===0 & counter%5===0){
+      //if the counter is divisible by both 3 and 5, display FOZZBAZZ
+      redirectTo: '/fozzbazz'
+    }
+  })
   .controller('fozzbazzCtrl', function(){});
