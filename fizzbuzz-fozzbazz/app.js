19d52cbfaf8e827b1856bf1a6761a3948db5884e
ENGMENJARI <anjari1981@gmail.com>
2017-02-04 10:03:42 +0200

    assess5

--- fizzbuzz-fozzbazz/app.js
+++ fizzbuzz-fozzbazz/app.js
@@ -1,6 +1,6 @@
 angular.module('app', ['ngRoute'])
   .config(function($routeProvider) {
-    var area={}
+    
     $routeProvider
       .when('/fizzbuzz', {
         templateUrl: 'partials/fizzbuzz.html',
@@ -20,38 +20,38 @@ angular.module('app', ['ngRoute'])
     are named what $routeProvider and the partials are expecting  */
 
   .factory('counter', function(){
-    var area={}
-    area.counter=0
-    area.display=''
+    
+    var counter=0;
+    
     //first Initialize counter with a value of 0
     
   })
   .controller('fizzbuzzCtrl', function(){
     //define a method to handle counter
-    area.increase=function(){
-      area.counter++
+   var increase=function(){
+      counter++
 
     }
     //if counter accept to be divided on three then I will display fizz
-    if(area.counter%3===0){
-      area.display='fizz';
-    }else if(area.counter%5===0){
+    if(counter%3===0){
+      redirectTo='fizzbuzz';
+    }else if(counter%5===0){
       //If the counter is divisible by five, display BUZZ
-      area.display='BUZZ'
-    }else if(area.counter%15===0){
+      redirectTo='fozzbazz'
+    }else if(counter%15===0){
       //if the counter is divisible by both 3 and 5, display FOZZBAZZ
-      area.display='FOZZBAZZ'
+      redirectTo='fozzbazz'
     }
   })
   .controller('fozzbazzCtrl', function(){
     //If the counter is divisible by four, display FOZZ
-    if(area.counter%4===0){
-      area.display='FOZZ';
-    } else if(area.counter%5===0){
-      area.display='BAZZ';
+    if(counter%4===0){
+      redirectTo='fozzbazz';
+    } else if(counter%5===0){
+      redirectTo=='fozzbazz';
       //If the counter is divisible by six, display BAZZ
-    }else if(area.counter%20===0){
-      redirectTo  :'FOZZBAZZ'
+    }else if(counter%20===0){
+      redirectTo  :'fozzbazz'
       //If the counter is divisible by both, display FOZZBAZZ
     }
   });
--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -9,7 +9,11 @@ var User = sequelize.define('User', {
 
 /*  Create a '/users' route that responds to 
     a GET request with all users in the database */
-
+app.get('/',function(req,res){
+	// when the reguest is get then 
+	// I have to display all users stored in my database
+	res.render('/user')
+})
 
 
 module.exports = { 
